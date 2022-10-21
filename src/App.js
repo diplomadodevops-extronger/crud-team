10222b32b81261f1bbfc83859dee580f35f669fa
extronger_dev <52896037+extrongerdev@users.noreply.github.com>
2022-10-20 22:38:32 -0300

    Adding text to the initial page. (#2)

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the template for our project.
-        </p>
+        <h1>Welcome to our site</h1>
+        <p>We are ready to help you</p>
         <a
           className="App-link"
           href="https://reactjs.org"
